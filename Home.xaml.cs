126a82a6186f120571877b3c209326b747c7021a
pooja-kamath <poojakamath91@gmail.com>
2019-02-08 15:54:44 +0530

    Create Home.xaml.cs

---
+++ Home.xaml.cs
@@ -0,0 +1,29 @@
+
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XYZ.Core.UILayer.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+      
+        public HomePage()
+        {
+          InitializeComponent();
+        }
+        private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+        }
+        private void TapGestureRecognizer_OnTappedd(object sender, EventArgs e)
+        {
+        }
+        private void TapGestureRecognizer_OnTappede(object sender, EventArgs e)
+        {
+        }
+        private void TapGestureRecognizer_OnTappedf(object sender, EventArgs e)
+        {
+        }
+    }
+}
